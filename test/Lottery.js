714cfe59e0bac2c569d888e585b3da2dad399ef8
santi1234567 <45318759+santi1234567@users.noreply.github.com>
2022-09-02 13:14:03 -0300

    Fixed tests

--- test/Lottery.js
+++ test/Lottery.js
@@ -64,7 +64,7 @@ describe("Lottery Contract", () => {
         
             });
             
-            it("Starts a lottery and NFT is transfered to lottery contract", async () => {
+            it("Starts a lottery successfully", async () => {
                 const { lotteryContract, nftContract, mockVRFContract, owner, addr1, addr2 } = await testSetup({});
         
                 // Mint an NFT
@@ -76,10 +76,24 @@ describe("Lottery Contract", () => {
   
                 // Start lottery
                 const bettingPrice = ethers.utils.parseEther("0.1"); // 0.1 ether
-                await lotteryContract.startLottery(nftId, nftContract.address, bettingPrice, owner.address);
-               
+                const tx = await lotteryContract.startLottery(nftId, nftContract.address, bettingPrice, owner.address);
+                const lotteryId = 0;
+                const timestamp = (await ethers.provider.getBlock(tx.blockNumber)).timestamp;
                 ownerOfMinted = await nftContract.ownerOf(nftId);           
-                await expect(ownerOfMinted).to.equal(lotteryContract.address);  
+                await expect(ownerOfMinted).to.equal(lotteryContract.address); 
+                 
+                lottery = await lotteryContract.getLottery(lotteryId);
+                await expect(lottery).to.eql(
+                    [
+                        owner.address, 
+                        nftContract.address,
+                        bettingPrice, 
+                        true, 
+                        [], 
+                        ethers.utils.parseEther("0"), 
+                        ethers.constants.AddressZero, 
+                        ethers.BigNumber.from( timestamp)
+                    ]);
             });
         });
 
@@ -96,15 +110,17 @@ describe("Lottery Contract", () => {
                 // Start lottery
                 const bettingPrice = ethers.utils.parseEther("0.1"); // 0.1 ether
                 await lotteryContract.startLottery(nftId, nftContract.address, bettingPrice, owner.address);
-
+                const lotteryId = 0;
                 // Buy ticket
-                //await lotteryContract.buyTicket();
-                expect(true).to.equal(false);
-        
-                // TODO: Check if ticket is bought
+                await lotteryContract.buyTicket(lotteryId, { value: bettingPrice});
+                [ , , , , players, lotteryBalance, , ] = await lotteryContract.getLottery(lotteryId);
+                expect(players).to.eql([owner.address]);  
+                expect(await ethers.provider.getBalance(lotteryContract.address)).to.equal(bettingPrice);   
+                expect(lotteryBalance).to.equal(bettingPrice);   
+
             });
         
-            it("Tries buying a ticket for a lottery without enough funds and fails", async () => {
+            it("Tries buying a ticket for a lottery with incorrect ticket price value", async () => {
                 const { lotteryContract, nftContract, mockVRFContract, owner, addr1, addr2 } = await testSetup({});
         
                 // Mint an NFT
@@ -114,20 +130,14 @@ describe("Lottery Contract", () => {
                 // Approve contract to be able to transfer the NFT
                 await nftContract.approve(lotteryContract.address, nftId);
                 // Start lottery
-                await lotteryContract.startLottery(nftId, nftContract.address);
-        
-                await owner.sendTransaction({
-                    to: addr1.address,
-                    value: ethers.utils.parseEther("9999.88"), // Depletes owner's ether
-                });
-        
-        
-                // Buy ticket
-                //expect(lotteryContract.buyTicket()).to.be.revertedWith(
-        //          "To participate, please fund the address with enough ether to buy the ticket."
-                //);
-                expect(true).to.equal(false);
-        
+                const bettingPrice = ethers.utils.parseEther("0.1"); // 0.1 ether
+                await lotteryContract.startLottery(nftId, nftContract.address, bettingPrice, owner.address);
+                const lotteryId = 0;
+                // Buy ticket        
+                await expect(lotteryContract.buyTicket(lotteryId, { value: ethers.utils.parseEther("0.12")})).to.be.revertedWith(
+                    "To participate, please add the required amount."
+                );   
+
             });   
         
             it("Buys tickets with different addresses and they are asigned correctly", async () => {
@@ -140,21 +150,24 @@ describe("Lottery Contract", () => {
                 // Approve contract to be able to transfer the NFT
                 await nftContract.approve(lotteryContract.address, nftId);
                 // Start lottery
-                await lotteryContract.startLottery(nftId, nftContract.address);
-                
+                const bettingPrice = ethers.utils.parseEther("0.1"); // 0.1 ether
+                await lotteryContract.startLottery(nftId, nftContract.address, bettingPrice, owner.address);
+                const lotteryId = 0;
+
                 // Buy ticket (owner)
-                //await lotteryContract.buyTicket();
+                await lotteryContract.buyTicket(lotteryId, { value: bettingPrice});
         
                 // Buy ticket (addr1)
-                //await lotteryContract.connect(addr1).buyTicket();
-        
+                await lotteryContract.connect(addr1).buyTicket(lotteryId, { value: bettingPrice});
+
                 // Buy ticket (addr2)
-                //await lotteryContract.connect(addr2).buyTicket();
+                await lotteryContract.connect(addr2).buyTicket(lotteryId, { value: bettingPrice});
         
-                
-                // TODO: Check if tickets were bought correctly
-                expect(true).to.equal(false);
-        
+          
+                [ , , , , players, lotteryBalance, , ] = await lotteryContract.getLottery(lotteryId);
+                expect(players).to.eql([owner.address, addr1.address, addr2.address]);  
+                expect(await ethers.provider.getBalance(lotteryContract.address)).to.equal(ethers.utils.parseEther("0.3"));   
+                expect(lotteryBalance).to.equal(ethers.utils.parseEther("0.3"));   
             });   
         
         
