6fb60a1212f84eaf35351f210834b83a622a9c30
santi1234567 <45318759+santi1234567@users.noreply.github.com>
2022-09-05 16:05:11 -0300

    Added URI to test nft

--- contracts/TestContracts/TestNFT.sol
+++ contracts/TestContracts/TestNFT.sol
@@ -3,13 +3,16 @@ pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract TestNFT is ERC721, ERC721Enumerable, Ownable {
+contract TestNFT is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
+    string TOKEN_URI_DEFAULT = "https://gateway.pinata.cloud/ipfs/QmeJHjTMhC3RqxMC1UQSnmQL1y7BwwVT431ugKFzCrh9du";
 
     constructor() ERC721("TestNFT", "TSTNFT") {}
 
@@ -17,6 +20,7 @@ contract TestNFT is ERC721, ERC721Enumerable, Ownable {
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(msg.sender, tokenId);
+        _setTokenURI(tokenId, TOKEN_URI_DEFAULT);
     }
 
 
@@ -36,5 +40,18 @@ contract TestNFT is ERC721, ERC721Enumerable, Ownable {
         returns (bool)
     {
         return super.supportsInterface(interfaceId);
+    }
+    
+        function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
     }
 }
